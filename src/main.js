b4c6e9f8f1b4775f0052e6d2870a328b07c2994b
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-07 22:12:23 +0100

    started router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
+
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  template: '<App/>',
+  components: { App }
 })
