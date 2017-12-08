00d5c717d41b5cea5ba5a2ebed39905e83c318c5
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-08 01:29:25 +0100

    updated general vue

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-
-// new Vue({
-//   el: '#app',
-//   render: h => h(App)
-// })
-
-
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -19,4 +10,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
