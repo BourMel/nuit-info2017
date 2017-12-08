94b6c9f2996404b39efbc7ab5ea6a97115ee5f04
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-08 07:42:51 +0100

    added some preventions links

--- src/components/Prevention.vue
+++ src/components/Prevention.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>Page prevention</h1>
+    <ul>
+      <li>
+        <a target="_blank" href="https://www.allodocteurs.fr/se-soigner/urgences/premiers-secours/urgences-comment-alerter-les-secours_12715.html">Urgences : comment alerter les secours?</a>
+      </li>
+      <li>
+        <a target="_blank" href="https://www.allodocteurs.fr/se-soigner/accidents/accidents-de-la-route/bien-reagir-face-a-un-accident-de-la-route_11682.html">Bien réagir face à un accident de la route</a>
+      </li>
+      <li>
+        <a target="_blank" href="https://www.allodocteurs.fr/se-soigner/accidents/accidents-de-la-route/accidents-de-la-route-prevenir-plutot-que-guerir_3091.html">Accidents de la route : prévenir plutôt que guérir</a>
+      </li>
+      <li>
+        <a target="_blank" href="https://www.allodocteurs.fr/se-soigner/urgences/premiers-secours/premiers-secours-savoir-reagir-en-cas-d-urgence_6975.html">Premiers secours : savoir réagir en cas d'urgence</a>
+      </li>
+    </ul>
   </div>
 </template>
 
