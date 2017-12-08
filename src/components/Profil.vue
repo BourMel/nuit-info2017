7a1b6822eb7074a001628c1acf007ff71b855f6c
Méline <meline.bl@outlook.fr>
2017-12-08 03:11:12 +0100

    titles

--- src/components/Profil.vue
+++ src/components/Profil.vue
@@ -3,6 +3,7 @@
     <h1>Page de profil</h1>
 
     <div id="cache" v-on:click="hide">
+      <p>{{ descriptif }}</p>
       <p>{{ message }}</p>
     </div>
 
@@ -51,32 +52,39 @@
     data() {
       return {
         message: '',
+        descriptif: '',
         radio: [
+          "Vous avez écouté la radio",
           "A levé le voile du silence",
           "Adepte des vibrations sonores",
           "Enivré.e de balado-diffusions"
         ],
         sam: [
+          "Vous avez été SAM",
           "A perdu à la courte paille",
           "Décidément malchanceux.se",
           "Altruiste abstinent"
         ],
         evenement: [
+          "Vous avez signalé des évènements",
           "Crieur de la chaussée",
           "Citoyen modèle",
           "Prophète accompli"
         ],
         conduite: [
+          "Vous avez utilisé le mode conduite",
           "Est sorti de chez soi et a découvert le monde",
           "Nomade responsable",
           "Globe-trotteur honorable"
         ],
         appli: [
+          "Vous avez lancé l'application",
           "Clic accidentel",
           "Curieux.se apprivoisé.e",
           "Pélerin dévoué"
         ],
         profil: [
+          "Vous avez consulté votre profil",
           "Premier coup d’oeil dans la glace",
           "Préoccupé.e par son image",
           "Egocentrique narcissique"
@@ -86,33 +94,39 @@
     methods: {
       display: function (e) {
         //afficher le bon niveau (bdd)
-        let level_radio = 0;
-        let level_sam = 0;
-        let level_evenement = 0;
-        let level_conduite = 0;
-        let level_appli = 0;
-        let level_profil = 0;
+        let level_radio = 1;
+        let level_sam = 1;
+        let level_evenement = 1;
+        let level_conduite = 1;
+        let level_appli = 1;
+        let level_profil = 1;
 
         document.getElementById("cache").style.display = 'block';
 
         switch(e.target.id) {
           case "radio":
             this.message = this.radio[level_radio];
+            this.descriptif = this.radio[0];
             break;
           case "sam":
             this.message = this.sam[level_sam];
+            this.descriptif = this.sam[0];
             break;
           case "evenement":
             this.message = this.evenement[level_evenement];
+            this.descriptif = this.evenement[0];
             break;
           case "conduite":
             this.message = this.conduite[level_conduite];
+            this.descriptif = this.conduite[0];
             break;
           case "appli":
             this.message = this.appli[level_appli];
+            this.descriptif = this.appli[0];
             break;
           case "profil":
             this.message = this.profil[level_profil];
+            this.descriptif = this.profil[0];
             break;
         }
       },
